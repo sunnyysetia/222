20717847f0fe1b2dbc76081f611edf7d39aa9dea
Sunny Setia <sunnyysetia@gmail.com>
2025-12-11 19:02:28 +1300

    added default random to books id

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,7 @@
 import { pgTable, uuid, text, timestamp, integer } from "drizzle-orm/pg-core";
 
 export const books = pgTable("books", {
-  id: uuid().primaryKey(),
+  id: uuid().primaryKey().defaultRandom(),
   title: text().notNull(),
   price: integer().notNull(),
   createdAt: timestamp().notNull().defaultNow(),
