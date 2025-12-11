7a2b4d5a4e7e9b3c194395e040a72e5207da66c2
Sunny Setia <sunnyysetia@gmail.com>
2025-12-11 19:19:11 +1300

    make api public

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/login"]);
+const isPublicRoute = createRouteMatcher(["/login", "/api(.*)", "/trpc(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
@@ -19,7 +19,5 @@ export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
   ],
 };
